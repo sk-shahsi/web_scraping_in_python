2f339eb86505c433ac484f4224efb2ec3563281d
sk-shahsi <sk.sashikumar055@gmail.com>
2025-11-28 11:53:34 +0530

    'content'

---
+++ downloding_images.py
@@ -0,0 +1,9 @@
+url = "https://media.istockphoto.com/id/814423752/photo/eye-of-model-with-colorful-art-make-up-close-up.jpg?s=612x612&w=0&k=20&c=l15OdMWjgCKycMMShP8UK94ELVlEGvt7GmB_esHWPYE="
+user ={
+        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+}
+response = requests.get(url=url, headers=user)
+pic = response.content
+
+f= open("")
+
--- web.py
+++ web.py
@@ -4,5 +4,8 @@ import requests
 
 url = 'http://127.0.0.1:8000/hello/'
 # print(dir(requests))
-response = requests.get(url=url)
-print(response.request.headers)
+user = {
+    "user-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+}
+response = requests.get(url=url, headers=user)
+print(response.content)
